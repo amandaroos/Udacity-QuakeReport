d4c9628a0abcde23d269137f7bd55d626658c1d4
Amanda Farrell <bluepandadev@gmail.com>
2017-01-24 13:31:57 -0700

    Implemented loadInBackground() using same code as doInBackground()

--- app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +22,13 @@ public class EarthquakeLoader extends AsyncTaskLoader <List<Earthquake>>{
 
     @Override
     public List<Earthquake> loadInBackground() {
-        return null;
+
+        //JSON query URL returns 10 most ercent earthquakes with at least a magnitude of 6
+        String EARTHQUAKE_URL = "http://earthquake.usgs.gov/fdsnws/event/1/" +
+                "query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+
+
+        ArrayList<Earthquake> result = QueryUtils.fetchEarthquakeData(EARTHQUAKE_URL);
+        return result;
     }
 }
